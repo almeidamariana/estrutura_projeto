ca9cda52e66a0a2bb3db27544fa937f88ba8634c
Mariana Almeida <marianath.almeida@gmail.com>
2023-11-24 21:33:00 -0300

    transform and load

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from pipeline.extract import extract_from_excel
+from pipeline.load import load_excel
 from pipeline.transform import contact_data_frames
-
 
 if __name__ == "__main__":
     data_frame_list = extract_from_excel("data/input")
     data_frame = contact_data_frames(data_frame_list)
-    print(data_frame)
-   
+    load_excel(data_frame, "data/output", "output")
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,17 +1,22 @@
 import pandas as pd
+
 from app.pipeline.transform import contact_data_frames
 
-df_1 = pd.DataFrame({'col1': [1,2], 'col2': [3,4]})
-df_2 = pd.DataFrame({'col1': [5,6], 'col2': [7,8]})
+df_1 = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+df_2 = pd.DataFrame({"col1": [5, 6], "col2": [7, 8]})
 
-def test_concat_dfs():
-    """ use o arrange act e assert para testar a função contact_data_frames. """
+
+def testar_a_concatenacao_da_lista_de_dataframe():
+    """use o arrange act e assert para testar a função contact_data_frames"""
 
     # arrange
-    df_list = [df_1, df_2]
+    data_frame_list = [df_1, df_2]
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
 
     # act
-    df = contact_data_frames(df_list)
+    df = contact_data_frames(data_frame_list)
 
     # assert
-    assert df.shape == (4,2)
+    assert df.shape == (4, 2)
+    assert data_frame.equals(df)
+    assert df.shape != (5, 2)
